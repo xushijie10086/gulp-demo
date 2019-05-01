c50328a46405f2599ec0465e3389cf60e4f41523
xushijie <shijie10086@163.com>
2019-05-01 14:41:46 +0800

    gulp simple demo
    gulp 3.9.1

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+// 注册任务
+gulp.task('renwuming', function() {
+    // 配置任务的操作
+    console.log('renwuming');
+    
+})
+
+//  注册默认任务
+gulp.task('default', ['renwuming'])
